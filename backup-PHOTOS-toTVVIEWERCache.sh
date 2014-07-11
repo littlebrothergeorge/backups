3309eccb1df2bafa4027354c8d83d4aa4fbb7eed
littlebrothergeorge <pmbohm+littlebrothergeorge@gmail.com>
2014-07-11 16:26:06 +1000

    document photos to crunchbang backup

--- backup-PHOTOS-toTVVIEWERCache.sh
+++ backup-PHOTOS-toTVVIEWERCache.sh
@@ -1,7 +1,8 @@
 
 ####################################
+# backup of the MEDIA drive to Crunchbang for viewing purposes
 # --dry-run -- delete
 
-rsync -ahir --delete  --exclude '.Trash-1000' --stats --progress /media/MEDIA/LightroomCatalog/ /media/Media/My\ Pictures/LightroomCatalog-backup/
-rsync -ahir --delete  --exclude '.Trash-1000' --stats --progress /media/MEDIA/My\ Pictures/OUR_PHOTOS/ /media/Media/My\ Pictures/OUR_PHOTOS/
+#rsync -ahir --delete  --exclude '.Trash-1000' --stats --progress /media/MEDIA/LightroomCatalog/ /media/Media/My\ Pictures/LightroomCatalog-backup/
+rsync -ahir --delete --exclude '.Trash-1000' --stats --progress /media/MEDIA/My\ Pictures/OUR_PHOTOS/ /media/Media/My\ Pictures/OUR_PHOTOS/
 
