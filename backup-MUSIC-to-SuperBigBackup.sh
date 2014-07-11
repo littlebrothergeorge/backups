7715ed19f5af5f5c86b253323eeda05cbf6cf339
littlebrothergeorge <pmbohm+littlebrothergeorge@gmail.com>
2014-07-11 15:58:37 +1000

    change author

--- backup-MUSIC-to-SuperBigBackup.sh
+++ backup-MUSIC-to-SuperBigBackup.sh
@@ -2,4 +2,4 @@
 
 
 # media/music to Super big backup
-rsync -ahir --delete  --exclude '.Trash-1000' --dry-run --exclude '$RECYCLE.BIN' --stats --progress /media/Media/My\ Music/ /media/SuperBigBackup/Media-backup/My\ Music/
+rsync -ahir --delete  --exclude '.Trash-1000' --exclude '$RECYCLE.BIN' --stats --progress /media/Media/My\ Music/ /media/SuperBigBackup/Media-backup/My\ Music/
