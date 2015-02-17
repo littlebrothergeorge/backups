26a68f35dd85ae8b1b2cc06ed9b363ed28e06edf
littlebrothergeorge <pmbohm+littlebrothergeorge@gmail.com>
2015-02-17 20:52:50 +1100

    edits

--- backup-PHOTOS-toSuperBigBackup.sh
+++ backup-PHOTOS-toSuperBigBackup.sh
@@ -2,6 +2,5 @@
 # backup of the MEDIA drive to SUPERBIGBACKUP from crunchbang 
 # --dry-run -- delete
 
-rsync -ahir --delete --dry-run --exclude '.Trash-1000' --stats --progress /media/MEDIA/LightroomCatalog/ /media/SuperBigBackup/Media-backup/My\ Pictures/LightroomCatalog-backup/
-rsync -ahir --delete --dry-run --exclude '.Trash-1000' --stats --progress /media/MEDIA/My\ Pictures/OUR_PHOTOS/ /media/SuperBigBackup/Media-backup/My\ Pictures/OUR_PHOTOS/
+rsync -ahir --delete --exclude '.Trash-1000' --stats --progress /media/MEDIA/My\ Pictures/ /media/SuperBigBackup/Media-backup/My\ Pictures/
 
